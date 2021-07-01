5437def8b9ff74ddcab29c0c84b594b97d7937ca
wanderleySilva-silva <jwps.17@hotmail.com>
2021-07-01 14:22:44 -0300

    Criação de entrada para as variáveis e foi editado o print.

--- MediaAritmetica.py
+++ MediaAritmetica.py
@@ -1,7 +1,12 @@
-m = 2
-n = 10
+m = int(input('Digite um número inteiro m: '))
+n = int(input('Digite um número inteiro n: '))
+
 
 soma = m + n
 media = soma/2
+
+rad = m**0.5
+
+print(rad)
 
 print('A média aritmética entre ' + str(m) + ' e '+ str(n) + ' é '+ str(media) + '.')
