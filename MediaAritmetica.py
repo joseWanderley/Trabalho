b276c6821f00c2f1394049e3123a787cca7737fb
joseWanderley <87076878+joseWanderley@users.noreply.github.com>
2021-07-07 10:14:10 -0300

    Criação de IF e ELSE.

--- MediaAritmetica.py
+++ MediaAritmetica.py
@@ -5,6 +5,12 @@ n = int(input('Digite um número inteiro n: '))
 soma = m + n
 media = soma/2
 
+if(media >= 6){
+  print("Média maior ou igual 6.")
+}else{
+  print("Média menor que 6.")
+}
+
 rad = m**0.5
 
 print(rad)
